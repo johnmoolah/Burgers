e3a405d91114809df7892ba469c79180ed96a43a
John Delizo <johndelizo99@gmail.com>
2021-02-28 05:58:26 -0800

    established seeds for database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, isDevoured) VALUES ("Cheeseburger", true);
+INSERT INTO burgers (name, isDevoured) VALUES ("Hamburger", true);
+INSERT INTO burgers (name, isDevoured) VALUES ("Veggieburger", true);
+
