466c9e11ae6cf19193ec6d61dfba87d4ac966ab7
John Delizo <johndelizo99@gmail.com>
2021-02-27 20:15:20 -0800

    installed handlebars and set as view engine

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+USE DATABASE burgers_db;
+CREATE TABLE burgers (
+    id 
+)
--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const PORT = 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//set handlebars as view engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 //Sets a basic route
 app.get('/', (req, res) => res.send('Hello World !'));
 
-//Listening on PORT
+//Listening on PORT 5000
 app.listen(PORT, () => console.log(`listeing on http://localhost:${PORT}`))
 
