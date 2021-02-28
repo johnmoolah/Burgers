e280e1d88e6f18289350003c98bdf95435e881c8
John Delizo <johndelizo99@gmail.com>
2021-02-27 20:43:39 -0800

    created mysql schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,9 @@
 DROP DATABASE IF EXISTS burgers_db;
 
-USE DATABASE burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 CREATE TABLE burgers (
-    id 
-)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    devoured BOOLEAN
+    );
